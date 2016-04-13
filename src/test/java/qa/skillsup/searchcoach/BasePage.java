e22cefd8cbee94b588de3f0d12bf6b053e1b61e0
Yan-Brav <yan.braverman37@gmail.com>
2016-04-13 18:00:35 +0300

    With changes

--- src/test/java/qa/skillsup/searchcoach/BasePage.java
+++ src/test/java/qa/skillsup/searchcoach/BasePage.java
@@ -10,11 +10,13 @@ import java.util.concurrent.TimeUnit;
 
 public class BasePage extends TestCase{
     protected static WebDriver driver;
+    private String baseUrl = "http://skillsup.ua/about/our-team.aspx#";
 
     @Before
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseUrl);
     }
 
     @After
--- src/test/java/qa/skillsup/searchcoach/SearchCoachIntoSkillsUpSiteTest.java
+++ src/test/java/qa/skillsup/searchcoach/SearchCoachIntoSkillsUpSiteTest.java
@@ -1,36 +1,21 @@
 package qa.skillsup.searchcoach;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 
 public class SearchCoachIntoSkillsUpSiteTest extends BasePage{
-    private String baseUrl;
+
     private String findCoach;
     private WebElement webElement;
-    //private List<WebElement> elenents = new ArrayList<WebElement>();
-    private String className = "member";
 
 
-    @Before
-    public void setUp(){
-        baseUrl = "http://skillsup.ua/about/our-team.aspx#";
-        driver.get(baseUrl);
-    }
 
     @Test
     public void testFindCoachJava()throws Exception{
         findCoach = "Александр Галковский";
-        webElement = driver.findElement(By.xpath("//span=Александр Галковский"));
-        //elenents = driver.findElements(By.className(className));
-        /*for (WebElement element : elenents){
-            //assertEquals(findCoach, element.findElement(By.linkText(findCoach)));
-            if (findCoach.equals(element.findElement(By.xpath("//span=Александр Галковский")))){
-                return;
-            }
-        }*/
+        webElement = driver.findElement(By.xpath("//span[text()='Александр Галковский']/.."));
 
        assertEquals(findCoach, webElement.getText());
     }

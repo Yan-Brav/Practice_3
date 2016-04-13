6851ddcf6734857d27753bd41866de905bc4376b
Yan-Brav <yan.braverman37@gmail.com>
2016-04-13 16:19:35 +0300

    With changes

---
+++ src/test/java/qa/skillsup/searchcoach/BasePage.java
@@ -0,0 +1,24 @@
+package qa.skillsup.searchcoach;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BasePage extends TestCase{
+    protected static WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        driver.quit();
+    }
+}

7756e3507cdb7c042f3e8954319c9da89b5c2f42
Yan-Brav <yan.braverman37@gmail.com>
2016-04-14 18:38:29 +0300

    With changes

--- src/test/java/qa/skillsup/searchcoach/SearchCoachIntoSkillsUpSiteTest.java
+++ src/test/java/qa/skillsup/searchcoach/SearchCoachIntoSkillsUpSiteTest.java
@@ -2,6 +2,7 @@ package qa.skillsup.searchcoach;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 
@@ -14,10 +15,24 @@ public class SearchCoachIntoSkillsUpSiteTest extends BasePage{
 
     @Test
     public void testFindCoachJava()throws Exception{
-        findCoach = "Александр Галковский";
-        webElement = driver.findElement(By.xpath("//span[text()='Александр Галковский']/.."));
+        findCoach = "http://skillsup.ua/media/22116/Alexander-Galkovskiy_P.jpg";
+        webElement = driver.findElement(By.cssSelector("img[src*='Galkovskiy']"));
 
-       assertEquals(findCoach, webElement.getText());
+       assertEquals(findCoach, webElement.getAttribute("src").toString());
+    }
+    @Test
+    public void testFindAdmin()throws Exception{
+        findCoach = "http://skillsup.ua/media/22166/Michael-Chokan_P.jpg";
+        webElement = driver.findElement(By.cssSelector("img[src*='Chokan']"));
+
+       assertEquals(findCoach, webElement.getAttribute("src").toString());
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void testFindCoachQAAutomation()throws NoSuchElementException{
+        findCoach = "http://skillsup.ua/media/22166/Artem-Karpov_P.jpg";
+        webElement = driver.findElement(By.cssSelector("img[src*='Karpov']"));
+
+       assertEquals(findCoach, webElement.getAttribute("src").toString());
     }
 
 
